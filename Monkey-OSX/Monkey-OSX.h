24068519e465ad518603569fe9da7e82181eec74
Joost Lubach <joost@yoazt.com>
2015-10-02 12:16:42 +0200

    Added OSX & watchOS targets

---
+++ Monkey-OSX/Monkey-OSX.h
@@ -0,0 +1,19 @@
+//
+//  Monkey-OSX.h
+//  Monkey-OSX
+//
+//  Created by Joost Lubach on 02/10/15.
+//  Copyright © 2015 Mos Dev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Monkey-OSX.
+FOUNDATION_EXPORT double Monkey_OSXVersionNumber;
+
+//! Project version string for Monkey-OSX.
+FOUNDATION_EXPORT const unsigned char Monkey_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Monkey_OSX/PublicHeader.h>
+
+
