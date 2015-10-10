3d05902c7252bb4281424e7ba91db61c32a953de
Joost Lubach <joost@yoazt.com>
2015-10-10 21:24:18 +0200

    REmoved build directory from git

--- build/Monkey.build/Release-iphoneos/Monkey-iOS.build/DerivedSources/Monkey_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char MonkeyVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Monkey  PROJECT:Monkey-1" "\n"; const double MonkeyVersionNumber __attribute__ ((used)) = (double)1.;
--- build/Monkey.build/Release-iphoneos/Monkey-iOS.build/Objects-normal/armv7/Monkey_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/joost/Work/Projects/OpenSource/Monkey/build/Monkey.build/Release-iphoneos/Monkey-iOS.build/DerivedSources/Monkey_vers.c
